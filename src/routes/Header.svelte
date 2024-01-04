f1148c791340fff6f3dab65d7c3af1e3a1434e74
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-04 18:48:22 +0100

    add header component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,130 +1,47 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
-	//console.log(logo);
+	import {
+		Button,
+		Alert,
+		Card,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Toast
+	} from 'flowbite-svelte';
+	import { CompressOutline } from 'flowbite-svelte-icons';
+
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+	<!-- <Button color="primary">Primary</Button>
+	<Alert color="primary">Primary</Alert> -->
+	<!-- <Card href="/cards" color="primary">
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-red-800">
+			Noteworthy technology acquisitions 2021
+		</h5>
+		<p class="font-normal leading-tight text-gray-700 dark:text-gray-700">
+			Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+			chronological order.
+		</p>
+	</Card> -->
+	<Navbar let:hidden let:toggle color="primary">
+		<NavBrand href="/">
+			<CompressOutline class="mx-2" />
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/posts">Posts</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/posts/new">Account</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-red-800"
+				>Fiona</span
+			>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
+			<NavLi href="/" active={true} aria-current={$page.url.pathname === '/' ? 'page' : undefined}>Home</NavLi>
+			<NavLi href="/posts">Blog</NavLi>
+			<NavLi href="/posts/new">Add Post</NavLi>
+			<NavLi href="/pricing">Account</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
