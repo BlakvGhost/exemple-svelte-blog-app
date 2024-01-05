fa2ede43a15c09ff07324b51740b62e8b87b5a56
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-05 23:38:08 +0100

    Configuring the keys needed for authentication from firebase

--- src/lib/firebase/firebase.app.ts
+++ src/lib/firebase/firebase.app.ts
@@ -4,26 +4,32 @@ import {
     PUBLIC_FIREBASE_PROJECT_ID,
     PUBLIC_FIREBASE_STORAGE_BUCKET,
     PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    PUBLIC_FIREBASE_APP_ID
-  } from '$env/static/public';
-  import { initializeApp, getApps } from 'firebase/app';
-  import { getAuth } from 'firebase/auth';
-  
-  const firebaseConfig = {
+    PUBLIC_FIREBASE_APP_ID,
+    PUBLIC_FIREBASE_MEASUREMENT_ID
+} from '$env/static/public';
+
+import { initializeApp, getApps } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
     apiKey: PUBLIC_FIREBASE_API_KEY,
     authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: PUBLIC_FIREBASE_APP_ID
-  };
-  
-  let firebaseApp;
-  
-  if (!getApps().length) {
+    appId: PUBLIC_FIREBASE_APP_ID,
+    measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+let firebaseApp;
+let firebaseAnalytics;
+
+if (!getApps().length) {
     firebaseApp = initializeApp(firebaseConfig);
-  }
-  
-  const firebaseAuth = getAuth(firebaseApp);
-  
-  export { firebaseApp, firebaseAuth };
+    firebaseAnalytics = getAnalytics(firebaseApp)
+}
+
+const firebaseAuth = getAuth(firebaseApp);
+
+export { firebaseApp, firebaseAuth, firebaseAnalytics };
