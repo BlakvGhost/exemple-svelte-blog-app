c37d836fd1d2129ec796e45572e3a42883bf5a71
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-05 01:49:37 +0100

    add img in h-screen on blog home page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,8 +5,10 @@
 <div class="">
 	<div class="w-full h-screen overflow-hidden">
 		<div class="h-full w-full" style="background-image: url('{authBgImg}');">
-			<div class="h-full w-full bg-slate-900/60">
-				
+			<div class="h-full w-full bg-slate-900/60 flex items-center px-4">
+				<div class="w-2/4">
+					<h1 class="text-6xl">Hello World.</h1>
+				</div>
 			</div>
 		</div>
 	</div>
