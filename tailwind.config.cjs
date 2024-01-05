9ca2c8ad4dd0bf3bf67bbe3dff3b6d26db762e84
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-05 01:54:22 +0100

    add Poppins font to project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,12 @@ const config = {
 				teal: { "50": "#f0fdfa", "100": "#ccfbf1", "200": "#99f6e4", "300": "#5eead4", "400": "#2dd4bf", "500": "#14b8a6", "600": "#0d9488", "700": "#0f766e", "800": "#115e59", "900": "#134e4a" },
 				cyan: { "50": "#ecfeff", "100": "#cffafe", "200": "#a5f3fc", "300": "#67e8f9", "400": "#22d3ee", "500": "#06b6d4", "600": "#0891b2", "700": "#0e7490", "800": "#155e75", "900": "#164e63" },
 			}
-		}
+		},
+		fontFamily: {
+			display: ['Poppins', 'Inter', 'system-ui', 'sans-serif'],
+			body: ['Poppins', 'Inter', 'system-ui', 'sans-serif'],
+			sans: ['Poppins', 'sans-serif'],
+		},
 	}
 };
 
