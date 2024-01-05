2fa210b7a252654df07bb226f0d7dc32ce5aee07
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-05 02:59:41 +0100

    add horizontal populars post to blog home page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,13 +1,39 @@
 <script>
 	import authBgImg from '$lib/images/auth-bg.jpg';
+	import Post from './Post.svelte';
 </script>
 
 <div class="">
-	<div class="w-full h-screen overflow-hidden">
+	<div class="h-screen w-full overflow-hidden">
 		<div class="h-full w-full" style="background-image: url('{authBgImg}');">
-			<div class="h-full w-full bg-slate-900/60 flex items-center px-4">
-				<div class="w-2/4">
-					<h1 class="text-6xl">Hello World.</h1>
+			<div class="flex h-full w-full items-center justify-between bg-slate-900/60 px-10">
+				<div class="mx-4 w-2/4">
+					<div class="w-fit bg-gray-900 p-2">
+						<h1>Robotics</h1>
+					</div>
+					<div class="my-3">
+						<h1 class="text-6xl">Hello World.</h1>
+					</div>
+					<div class="my-6">
+						<button
+							class="my-4 border bg-transparent p-3 font-bold
+							transition hover:border-primary-900 hover:bg-primary-900">Continue Reading</button
+						>
+					</div>
+				</div>
+				<div class="my-5 w-1/4 overflow-hidden">
+					<div class="container">
+						<div class="w-full border-b-2 py-2">
+							<h1 class="text-2xl">Populars Posts</h1>
+						</div>
+						<div class="my-2">
+							<div class="flex w-full flex-col gap-3 overflow-auto">
+								{#each Array(2) as _, index}
+									<Post type={true} custom_class="w-full bg-primary-600 border-0 text-white"></Post>
+								{/each}
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
---
+++ src/routes/(app)/Post.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Card, Button } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	export let type: boolean;
+
+	export let custom_class: string = '';
+</script>
+
+<Card class={custom_class}>
+	{#if type}
+		<h5 class="mb-2 font-bold tracking-tight text-gray-300 dark:text-white">
+			Noteworthy technology acquisitions 2021
+		</h5>
+		<p class="mb-3 font-normal leading-tight text-gray-900 dark:text-gray-400">
+			Here are the biggest enterprise....
+		</p>
+		<Button class="w-fit">
+			Read more <ArrowRightOutline class="ms-2 h-3.5 w-3.5 text-white" />
+		</Button>
+	{:else}
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			Noteworthy technology acquisitions 2021
+		</h5>
+	{/if}
+</Card>
