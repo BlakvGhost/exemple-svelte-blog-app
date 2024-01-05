bf41e0e362325fe7ce4b8352a0002df4cf18ad70
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-05 16:13:32 +0100

    Addition of style necessary to make the blog home page responsive

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -7,7 +7,7 @@
 	<div class="h-screen w-full overflow-hidden">
 		<div class="h-full w-full" style="background-image: url('{authBgImg}');">
 			<div class="flex h-full w-full items-center justify-between bg-slate-900/60 px-10">
-				<div class="mx-4 w-2/4">
+				<div class="mx-4 w-full md:w-2/4">
 					<div class="w-fit bg-gray-900 p-2">
 						<h1>Robotics</h1>
 					</div>
@@ -21,7 +21,7 @@
 						>
 					</div>
 				</div>
-				<div class="my-5 w-1/4 overflow-hidden">
+				<div class="my-5 hidden w-1/4 overflow-hidden md:block">
 					<div class="container">
 						<div class="w-full border-b-2 py-2">
 							<h1 class="text-2xl">Populars Posts</h1>
@@ -38,11 +38,21 @@
 			</div>
 		</div>
 	</div>
-	<div class="container mx-auto">
+	<div class="md:-p container mx-auto block px-3 md:hidden">
+		<div class="py-3">
+			<h1 class="border-b py-3 text-2xl">Populars Posts</h1>
+		</div>
+		<div class="my-4 flex flex-col items-center gap-4">
+			{#each Array(2) as _, index}
+				<Post type={true} custom_class="bg-primary-600 w-full border-0 text-white"></Post>
+			{/each}
+		</div>
+	</div>
+	<div class="md:-p container mx-auto px-3">
 		<div class="py-3">
 			<h1 class="border-b py-3 text-2xl">Recents Posts</h1>
 		</div>
-		<div class="my-4 grid grid-cols-4 gap-4">
+		<div class="my-4 grid grid-cols-2 gap-4 md:grid-cols-4">
 			{#each Array(10) as _, index}
 				<Post type={false}></Post>
 			{/each}
