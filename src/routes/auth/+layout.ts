e716b9da0eec6398f3eb443913835f3f5c01645c
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-06 03:34:06 +0100

    add guard to prevent authenticated users on auth pages

---
+++ src/routes/auth/+layout.ts
@@ -0,0 +1,15 @@
+import { authUser } from '$lib/authStore';
+import { redirect } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async () => {
+    const unsubscribe = authUser.subscribe((user) => {
+        if (user) {
+            throw redirect(302, '/');
+        }
+    });
+
+    unsubscribe();
+
+    return {};
+};
