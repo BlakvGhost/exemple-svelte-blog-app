7cb678bb91351424c52e2ca4d37dba8f49c5e4e0
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-06 05:55:44 +0100

    Add a bind to reactive var for firebase

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
+	import { login as lg } from '$lib/auth.service';
+	import { EMPTY_FIELDS_MESSAGE, PASSWORD_NOT_SAME_MESSAGE } from '$lib/message';
+	import { Alert } from 'flowbite-svelte';
+	import { InfoCircleSolid } from 'flowbite-svelte-icons';
+
+	let email = '';
+	let password = '';
+
+	let error: string | undefined;
+
+	let process = false;
+
+	$: {
+		email;
+		password;
+		error;
+		process;
+	}
+
+	async function login(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
+		if (email && password) {
+			process = true;
+			error = await lg(email, password);
+			return (process = false);
+		}
+		return (error = EMPTY_FIELDS_MESSAGE);
+	}
 </script>
 
-
-<form class="space-y-5">
+{#if error}
+	<Alert color="red" border>
+		<InfoCircleSolid slot="icon" class="h-4 w-4" />
+		<span class="font-medium">{error}</span>
+	</Alert>
+{/if}
+<form class="space-y-5" on:submit|preventDefault={login}>
 	<div>
 		<label for="email" class="font-medium">Email</label>
 		<input
+			bind:value={email}
 			id="email"
 			type="email"
 			required
@@ -15,15 +48,27 @@
 	<div>
 		<label for="password" class="font-medium">Password</label>
 		<input
+			bind:value={password}
 			id="password"
 			type="password"
 			required
 			class="w-full rounded-lg border bg-transparent px-3 py-2 text-gray-500 shadow-sm outline-none focus:border-indigo-600"
 		/>
 	</div>
-	<button
-		class="w-full rounded-lg bg-indigo-600 px-4 py-2 font-medium text-white duration-150 hover:bg-indigo-500 active:bg-indigo-600"
-	>
-		Sign in
-	</button>
+	{#if process}
+		<button
+			type="button"
+			class="w-full rounded-lg bg-indigo-400 px-4 py-2 font-medium text-white"
+			disabled
+		>
+			<span class="mr-3 h-5 w-5 animate-spin"> </span>
+			Processing...
+		</button>
+	{:else}
+		<button
+			class="w-full rounded-lg bg-indigo-600 px-4 py-2 font-medium text-white duration-150 hover:bg-indigo-500 active:bg-indigo-600"
+		>
+			Sign In
+		</button>
+	{/if}
 </form>
