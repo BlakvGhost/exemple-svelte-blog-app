e429f0f48e6c0aeb9ffdd28f73f1cf4d38acfb12
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-07 07:28:14 +0100

    Add dynamic logic create post

--- src/routes/(app)/posts/new/+page.server.ts
+++ src/routes/(app)/posts/new/+page.server.ts
@@ -1,5 +1,4 @@
-import { Category } from '$lib/category/category';
-import { create, getAll as getAllCategories } from '$lib/category/category.service';
+import { getAll as getAllCategories } from '$lib/category/category.service';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
@@ -12,10 +11,6 @@ export const load: PageServerLoad = async () => {
         desc: category.desc,
         created_at: category.created_at,
     }));
-
-    // create(new Category(undefined, 'Math', 'Les merveilles de la Math')).then((res) => {
-    //     console.log(res);
-    // });
 
     return {
         categories: serializedCategories,
